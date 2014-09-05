eedf9c42d4bc8145cb6160bb690f1f62b0a05392
kaestel <kaestel@e-types.com>
2014-09-05 15:31:51 +0200

    updated

--- src/templates/pages/front.php
+++ src/templates/pages/front.php
@@ -1,12 +1,23 @@
 <div class="scene front i:front">
+
+	<ul class="slideshow">
+		<li><img src="/img/pi_1.jpg"></li>
+	</ul>
+
 	<h1>Velkommen til Circle of Life</h1>
-
-	<p class="todo">Her kommer et slideshow af en art</p>
-
-	<img src="../img/pi_dorthe.jpg" alt="Dorthe Torp" style="float: right; margin: 0 0 0 10px" />
-
-	<p class="todo">
-		DORTHE: Her skal vi have en tekst, hvor du skriver noget direkte om hvad du laver. 
-		Det er vigtigt at de behandlingsformer du tilbyder nævnes, gerne flere gange, dog uden at det bliver for meget.
+	<p>
+		På klinikken arbejder jeg med <a href="/pages/tankefeltterapi">EFT/TFT</a>, 
+		<a href="/pages/healing">Healing</a> og <a href="/pages/tarotkort">Tarotkort</a>. 
+	</p>
+	<p>
+		TFT og EFT er forkortelser for Tankefeltterapi og følelsesmæssig terapi (emotionel terapi). 
+		TFT &amp; EFT hjælper ophæver blokeringer der kommer af svigt, angst, vrede, sorg, mindreværd, 
+		Post traumatisk stress o.s.v.
+	</p>
+	<p>
+		Reikihealing hjælper mod stress, dårlig ryg, smerter. Det kan udrense giftstoffer og give total afsp&aelig;nding.
+	</p>
+	<p>
+		Tarotkort hjælper dig til at få afklaret livets problemer og hvad du kan gøre ved dem.
 	</p>
 </div>
