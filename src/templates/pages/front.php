c3ad8dbcee030b0708a3d93b057c0491c09fdd41
kaestel <kaestel@e-types.com>
2015-01-02 22:51:01 +0800

    updated frontpage

--- src/templates/pages/front.php
+++ src/templates/pages/front.php
@@ -8,19 +8,24 @@
 
 	<div class="description" itemprop="articleBody">
 		<p>
-			På klinikken arbejder jeg med <a href="/pages/tankefeltterapi">EFT/TFT</a>, 
-			<a href="/pages/healing">Healing</a> og <a href="/pages/tarotkort">Tarotkort</a>. 
+			På min klinik i Svendborg og Odense her på Fyn arbejder jeg med <a href="/pages/tankefeltterapi">EFT og TFT</a>
+			samt <a href="/pages/healing">Healing</a> og <a href="/pages/tarotkort">Tarotkort</a>. 
+			I Odense har jeg klinik på Oasen og i Svendborg har jeg klinik på Rolf Krakes Vej.
 		</p>
 		<p>
+			EFT og TFT er effektiv til at afhjælpe Post Traumatisk Stress, angst, forbier, vrede, sorg og svigt. 
 			TFT og EFT er forkortelser for Tankefeltterapi og følelsesmæssig terapi (emotionel terapi). 
-			TFT &amp; EFT hjælper ophæver blokeringer der kommer af svigt, angst, vrede, sorg, mindreværd, 
-			Post traumatisk stress o.s.v.
+			TFT og EFT hjælper med at ophæve følelsesmæssige blokeringer, der kommer af svigt, angst, vrede, 
+			sorg, mindreværd, post traumatisk stress (ptsd) o.s.v.
 		</p>
 		<p>
-			Reikihealing hjælper mod stress, dårlig ryg, smerter. Det kan udrense giftstoffer og give total afsp&aelig;nding.
+			Reikihealing / Healing er noget af det mest afstressende du kan give dig selv og din krop. 
+			Desuden hjælper Healing mod smerter og hjælper din krop med at udrense giftstoffer.
 		</p>
 		<p>
-			Tarotkort hjælper dig til at få afklaret livets problemer og hvad du kan gøre ved dem.
+			Tarotkort er Sjælens spejl og du kan spørge kortene om alt og få svar. Tarot hjælper dig til 
+			at få afklaret livets store og små problemer og hvordan du håndterer dem.
 		</p>
+
 	</div>
 </div>
