1c023b2fd48be61f420ad669edffa3ec777c2a23
Martin Kæstel Nielsen <martin@think.dk>
2018-07-03 10:09:08 +0200

    Update

--- theme/www/js/seg_mobile.js
+++
@@ -1,8 +0,0 @@
-
-/*seg_mobile_include.js*/
-
-/*i-unsupported.js*/
-function unsupported() {
-	document.body.innerHTML = '<h1 style="text-align: center; margin: 20% 15% 5%; font-family: Arial; color: #333333">Denne side virker desværre ikke på din computer/telefon</h1><p style="text-align: center; margin: 2% 15% 5%; font-family: Arial; color: #333333">Dorthe Torp, Healing, Clairvoyance og Tarotkort. Telefon 4092 0496.</p>';
-}
-window.onload = unsupported;
