b92f3e9a58071d39d7907441dec9bf7da7928583
Martin Kæstel Nielsen <martin@think.dk>
2018-07-03 10:25:44 +0200

    Minor adjustments

--- theme/templates/pages/404.php
+++ theme/templates/pages/404.php
@@ -2,6 +2,6 @@
 	<h1>404</h1>
 	<h2>Vi kan ikke finde siden</h2>
 	<p>
-		Måske er der sket en fejl og vi vil gerne høre fra dig. Send en mail til <a href="mailto:martin@kaestel.dk?subject=Page%20missing:<?= $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] ?>">martin@kaestel.dk</a>.
+		Måske er der sket en fejl – og vi vil gerne høre fra dig, så vi kan få rettet den. Send en mail til <a href="mailto:martin@kaestel.dk?subject=Page%20missing:<?= $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] ?>">martin@kaestel.dk</a>.
 	</p>
 </div>
