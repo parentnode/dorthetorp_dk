bb77b1bc8ee0efc6ba4e883fe3e362a6004a0dde
Martin Kæstel Nielsen <martin@think.dk>
2018-07-07 14:01:03 +0200

    Update license and remove old manipulator

--- theme/www/js/manipulator/v0_9_2-dorthetorp_dk/merged/seg_smartphone.js
+++ theme/www/js/manipulator/v0_9_2-dorthetorp_dk/merged/seg_smartphone.js
@@ -1,6 +1,6 @@
 /*
-Manipulator v0.9.2-axpoint Copyright 2018 http://manipulator.parentnode.dk
-js-merged @ 2018-07-07 11:14:46
+Manipulator v0.9.2-dorthetorp_dk Copyright 2018 http://manipulator.parentnode.dk
+js-merged @ 2018-07-07 02:00:07
 */
 
 /*seg_smartphone_include.js*/
